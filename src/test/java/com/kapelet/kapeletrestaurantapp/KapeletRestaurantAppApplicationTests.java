77447c71b76fc10576383cf980d58f83e2802e62
Kelvin <kelvindervishllari@gmail.com>
2022-10-09 20:10:07 +0200

    init commit

---
+++ src/test/java/com/kapelet/kapeletrestaurantapp/KapeletRestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapelet.kapeletrestaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapeletRestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
